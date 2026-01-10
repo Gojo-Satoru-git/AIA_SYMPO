e8a24bd0cf7fae3d81ef668817bc359e97711116
GrandWizard1102 <v.kavimani1106@gmail.com>
2026-01-10 16:34:39 +0530

    Event-page

--- sympo/src/screens/Events.jsx
+++ sympo/src/screens/Events.jsx
@@ -1,7 +1,102 @@
+import React, { useState } from 'react';
+import Eventcard from '../components/Eventcard';
 const Events = () => {
+  const [Selected, SetSelected] = useState('All');
+  const eventext = [
+    {
+      title: 'Event1',
+      id: '1',
+      image: '/event1.png',
+      description: 'This is event 1',
+      category: 'Technical',
+      date: '2026-02-21',
+      time: '10:00 AM',
+    },
+    {
+      title: 'Event2',
+      id: '2',
+      image: '/event2.png',
+      description: 'This is event 2',
+      category: 'Technical',
+      date: '2026-02-21',
+      time: '10:00 AM',
+    },
+    {
+      title: 'Event3',
+      id: '3',
+      image: '/event3.png',
+      description: 'This is event 3',
+      category: 'Technical',
+      date: '2026-02-21',
+      time: '10:00 AM',
+    },
+    {
+      title: 'EventA',
+      id: '4',
+      image: '/eventA.png',
+      description: 'This is event A',
+      category: 'Non-Technical',
+      date: '2026-02-21',
+      time: '10:00 AM',
+    },
+    {
+      title: 'EventB',
+      id: '5',
+      image: '/eventB.png',
+      description: 'This is event B',
+      category: 'Non-Technical',
+      date: '2026-02-21',
+      time: '10:00 AM',
+    },
+    {
+      title: 'EventC',
+      id: '6',
+      image: '/eventC.png',
+      description: 'This is event C',
+      category: 'Non-Technical',
+      date: '2026-02-21',
+      time: '10:00 AM',
+    },
+  ];
+  const display =
+    Selected === 'All' ? eventext : eventext.filter((event) => event.category === Selected);
   return (
-    <div className="flex min-h-screen justify-center items-center flex-1 text-white">
-      <p>Events</p>
+    <div className="flex flex-col items-center p-10 bg-black">
+      <div className="flex flex-grow justify-center items-start gap-4">
+        {display.map((events) => (
+          <Eventcard
+            key={events.id}
+            title={events.title}
+            desc={events.description}
+            img={events.image}
+            date={events.date}
+            time={events.time}
+          />
+        ))}
+      </div>
+
+      <div className="flex  justify-center items-center py-10 text-primary">
+        <ul className="flex justify-center gap-8">
+          <li
+            className={` p-2 rounded-full hover:animate-popup border-solid animated-border animate-fade-in-down shadow-stGlow cursor-pointer ${Selected === 'All' ? 'bg-primary text-black' : ''}`}
+            onClick={() => SetSelected('All')}
+          >
+            All
+          </li>
+          <li
+            className={`p-2 rounded-full hover:animate-popup border-solid animated-border animate-fade-in-down shadow-stGlow cursor-pointer ${Selected === 'Technical' ? 'bg-primary text-black' : ''}`}
+            onClick={() => SetSelected('Technical')}
+          >
+            Technical
+          </li>
+          <li
+            className={` p-2 rounded-full hover:animate-popup border-solid animated-border animate-fade-in-down shadow-stGlow cursor-pointer ${Selected === 'Non-Technical' ? 'bg-primary text-black' : ''}`}
+            onClick={() => SetSelected('Non-Technical')}
+          >
+            Non-Technical
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
--- sympo/tailwind.config.js
+++ sympo/tailwind.config.js
@@ -1,22 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-       fontFamily: {
+      fontFamily: {
         stranger: ['"StrangerThings"', 'serif'],
         strange: ['"StrangerRegular"', 'serif'],
       },
       colors: {
-        primary: "#E50914",       
-        dark: "#0B0B0B",          
-        darkCard: "#111111",
+        primary: '#E50914',
+        dark: '#0B0B0B',
+        darkCard: '#111111',
       },
 
       boxShadow: {
-        stGlow: "0 0 20px rgba(229,9,20,0.6)",
-        stGlowStrong: "0 0 30px rgba(229,9,20,0.9)",
-        
+        stGlow: '0 0 20px rgba(229,9,20,0.6)',
+        stGlowStrong: '0 0 30px rgba(229,9,20,0.9)',
       },
 
       keyframes: {
@@ -24,38 +23,40 @@ export default {
           '0%, 19%, 21%, 23%, 25%, 54%, 56%, 100%': { opacity: '1' },
           '20%, 22%, 24%, 55%': { opacity: '0.4' },
         },
-      
-          zoomIn: {
-    "0%": {
-      transform: "scale(5)",
-      opacity: "0",
-    },
-    "100%": {
-      transform: "scale(1)",
-      opacity: "1",
-    },
-  },
-        
+        popup: {
+          '0%': { transform: 'scale(1)', opacity: '0' },
+          '100%': { transform: 'scale(1.25)', opacity: '1' },
+        },
+        zoomIn: {
+          '0%': {
+            transform: 'scale(5)',
+            opacity: '0',
+          },
+          '100%': {
+            transform: 'scale(1)',
+            opacity: '1',
+          },
+        },
+
         fadeInDown: {
-          "0%": {
-            opacity: "0",
-            transform: "translateY(-20%)",
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-20%)',
           },
-          "100%": {
-            opacity: "1",
-            transform: "translateY(0)",
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
           },
-        },                                        
+        },
       },
 
       animation: {
-        "fade-in-down": "fadeInDown 0.8s ease-in-out",
-        flicker: "flicker 2s infinite",
-       
-        zoom: "zoomIn 1.8s ease-out forwards",
+        'fade-in-down': 'fadeInDown 0.8s ease-in-out',
+        flicker: 'flicker 2s infinite',
+        popup: 'popup 0.5s ease-out forwards',
+        zoom: 'zoomIn 1.8s ease-out forwards',
       },
     },
   },
   plugins: [],
 };
-
