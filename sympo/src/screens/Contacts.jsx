35a559c43397c3ac1f175d0179769685fd82a9aa
naveen <naveenkumar03202006@gmail.com>
2026-01-10 12:26:56 +0530

    updated in contact page

--- sympo/src/screens/Contacts.jsx
+++ sympo/src/screens/Contacts.jsx
@@ -74,7 +74,7 @@ const Contacts = () => {
               pb-6
               scroll-smooth
               hide-scrollbar
-
+              rounded-lg
               px-4 sm:px-6 md:px-0
               py-2
             "
@@ -179,7 +179,7 @@ const Contacts = () => {
             overflow-x-auto
             pb-6
             hide-scrollbar
-
+            rounded-md
             px-4
             py-2.5
 
