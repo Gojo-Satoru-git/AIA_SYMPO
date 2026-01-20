662d67727c7c3bb27e268ea891315134ee86dbee
ragavendra0604 <2023510031@student.annauniv.edu>
2026-01-20 22:56:46 +0530

    import fix in main.jsx

--- sympo/src/main.jsx
+++ sympo/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx';
-import "./src/App.css";
+import "./App.css";
 import theme from "./theme";
 import { AuthProvider } from "./context/AuthContext";
 import { ThemeProvider } from "@mui/material/styles";
