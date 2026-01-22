f27bdc6c8382c1a5f4cd80c96db884b54db134da
Gojo-Satoru-git <praveenchandru1209@gmail.com>
2026-01-22 13:17:19 +0530

    Razor pay service

--- sympo/backend/src/service/razorpay.service.js
+++ sympo/backend/src/service/razorpay.service.js
@@ -5,33 +5,37 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const instance = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_KEY_SECRET,
+  key_id: process.env.VITE_RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
 // Function 1: Ask Razorpay to create a pending order
 export const createRazorpayOrder = async (amount) => {
-    const option = {
-        amount: amount * 100,
-        currency: "INR",
-        receipt: `receipt_${Date.now()}`,
-    };
+  const option = {
+    amount: amount * 100,
+    currency: 'INR',
+    receipt: `receipt_${Date.now()}`,
+  };
 
-    try {
-        const order = await instance.orders.create(option);
-        return order;
-    }catch(error) {
-        throw error;
-    }
+  try {
+    const order = await instance.orders.create(option);
+    return order;
+  } catch (error) {
+    throw error;
+  }
 };
 
-export const verifyRazorpayPayment = (razorpay_order_id, razorpay_payment_id, razorpay_signature) => {
-  const body = razorpay_order_id + "|" + razorpay_payment_id;
+export const verifyRazorpayPayment = (
+  razorpay_order_id,
+  razorpay_payment_id,
+  razorpay_signature
+) => {
+  const body = razorpay_order_id + '|' + razorpay_payment_id;
 
   const expectedSignature = crypto
-    .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
+    .createHmac('sha256', process.env.RAZORPAY_KEY_SECRET)
     .update(body.toString())
-    .digest("hex");
+    .digest('hex');
 
   return expectedSignature === razorpay_signature;
 };
