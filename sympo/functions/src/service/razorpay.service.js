19c6302110ed7857c91a6b9b1cb8b0d3ab31a925
Rahul-Sundarraj <fftgosr@gmail.com>
2026-01-26 12:17:23 +0530

    modified pull request

--- sympo/functions/src/service/razorpay.service.js
+++ sympo/functions/src/service/razorpay.service.js
@@ -1,7 +1,7 @@
 import crypto from 'crypto';
 import Razorpay from 'razorpay';
 
-import { RAZORPAY_KEY_ID, RAZORPAY_KEY_SECRET } from '../config/env1.js';
+import { RAZORPAY_KEY_ID, RAZORPAY_KEY_SECRET } from '../config/env.js';
 
 
 if (!RAZORPAY_KEY_ID || !RAZORPAY_KEY_SECRET) {
