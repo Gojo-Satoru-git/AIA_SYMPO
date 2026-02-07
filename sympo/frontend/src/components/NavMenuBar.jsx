3cca7b0fd98d8cd347b53512c04b52f63cd567af
GrandWizard1102 <v.kavimani1106@gmail.com>
2026-02-07 20:03:23 +0530

    Styling scan2

--- sympo/frontend/src/components/NavMenuBar.jsx
+++ sympo/frontend/src/components/NavMenuBar.jsx
@@ -183,7 +183,7 @@ const NavMenubar = ({ HomeRef, AboutRef, EventsRef, ContactRef, FAQsRef, Registe
             <button
               onClick={() => navigate('/qr-scanner')}
               className="
-                flex items-center justify-center gap-2 
+                flex items-center justify-center
                 px-4 py-2 
                 bg-primary 
                 text-white   
