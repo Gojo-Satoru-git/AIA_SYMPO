3136c8548d6dfb94c2f14507b8ec987c77882910
ragavendra0604 <2023510031@student.annauniv.edu>
2026-02-07 10:06:16 +0530

    API fetch fix

--- sympo/frontend/src/services/api.js
+++ sympo/frontend/src/services/api.js
@@ -84,7 +84,7 @@ export const searchColleges = async (search) => {
 };
 
 export const sendOtpApi = async (email) => {
-  const response = await api.post("/otp/send--oombu", { email });
+  const response = await api.post("/otp/send", { email });
   return response.data;
 }
 
